b07f7849c894967ec51b1621a50a1be06f9a8531
Franklin <franklin_daniel2000@hotmail.com>
2024-08-20 23:41:27 -0700

    feat -h(Initial Project)

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace DilanLanding.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult AutonomoFitness()
+        {
+            return View();
+        }
+
+        public IActionResult Progresivo()
+        {
+            return View();
+        }
+
+        public IActionResult TransformacionTotal()
+        {
+            return View();
+        }
+    }
+}
